c39b8cb7ba0b2f5b9d71882fbc8ff70b056d6f68
kgyeonn <kgyeonn@gmail.com>
2021-05-15 17:19:19 +0900

    기존 포인트 화면 삭제

--- app/src/main/java/com/example/finalproject/PointFragment.java
+++ app/src/main/java/com/example/finalproject/PointFragment.java
@@ -7,15 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class PointFragment extends Fragment {
+
+    private ListView point_lv;
+    private ArrayList<PointVO> data;
+    private PointViewHoler adapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fragment = inflater.inflate(R.layout.fragment_point, container, false);
+
 
         return fragment;
     }

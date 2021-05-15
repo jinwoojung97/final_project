9a9b1b3127280a66ad6edec3297cede8937fabfc
kgyeonn <kgyeonn@gmail.com>
2021-05-15 11:47:09 +0900

    로그인 화면 커밋

---
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btn_login, btn_join;
+    EditText edit_id, edit_pw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edit_id = findViewById(R.id.edit_id);
+        edit_pw = findViewById(R.id.edit_pw);
+        btn_login = findViewById(R.id.btn_login);
+        btn_join = findViewById(R.id.btn_join);
+
+
+        // 로그인 버튼 클릭 시 로그인 실패/성공 팝업창 띄우기
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 회원가입 버튼 클릭 시 회원가입 페이지로 전환
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}

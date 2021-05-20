6ad85265157b37fceeec7be01feb3665a02be031
jinwoo-jung <wlsdn4808@naver.com>
2021-05-20 10:49:58 +0900

    마이페이지, 조인페이지 레이아웃 수정

--- app/src/main/java/com/example/finalproject/MyPageFragment.java
+++ app/src/main/java/com/example/finalproject/MyPageFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -19,7 +20,7 @@ import java.util.ArrayList;
 public class MyPageFragment extends Fragment {
 
     TextView tv_mypage_id, tv_mypage_point;
-    TextView tv_mypage_pw, tv_mypage_phone, tv_mypage_region;
+    EditText edit_mypage_pw, edit_mypage_phone, edit_mypage_region;
     Button btn_mypage_point;
 
     @Override
@@ -30,9 +31,9 @@ public class MyPageFragment extends Fragment {
 
         tv_mypage_id = fragment.findViewById(R.id.tv_mypage_id);
         tv_mypage_point = fragment.findViewById(R.id.tv_mypage_point);
-        tv_mypage_pw = fragment.findViewById(R.id.tv_mypage_pw);
-        tv_mypage_phone = fragment.findViewById(R.id.tv_mypage_phone);
-        tv_mypage_region = fragment.findViewById(R.id.tv_mypage_region);
+        edit_mypage_pw = fragment.findViewById(R.id.edit_mypage_pw);
+        edit_mypage_phone = fragment.findViewById(R.id.edit_mypage_phone);
+        edit_mypage_region = fragment.findViewById(R.id.edit_mypage_region);
         btn_mypage_point = fragment.findViewById(R.id.btn_mypage_point);
 
         // 회원테이블에서 아이디  가져오기

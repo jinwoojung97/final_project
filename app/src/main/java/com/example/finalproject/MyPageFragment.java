67093483806d4992a651e111188319776e6eaf33
joonTeob <jjy8256@gamil.com>
2021-05-21 18:00:13 +0900

    마이페이지 초기화

--- app/src/main/java/com/example/finalproject/MyPageFragment.java
+++ app/src/main/java/com/example/finalproject/MyPageFragment.java
@@ -2,46 +2,17 @@ package com.example.finalproject;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class MyPageFragment extends Fragment {
-    private DatabaseReference reference;
     String member_id, member_pw, member_phone, member_reg;
-
-    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference  databaseReference = firebaseDatabase.getReference();
-
-    static ArrayList<String> arrayListID = new ArrayList<String>();
-    static ArrayList<String> arrayListPW = new ArrayList<String>();
-
-    static ArrayList<String> arrayIndex = new ArrayList<String>();
 
     TextView tv_mypage_id, tv_mypage_point;
     EditText edit_mypage_pw, edit_mypage_phone, edit_mypage_region;
@@ -79,59 +50,9 @@ public class MyPageFragment extends Fragment {
                 member_pw = edit_mypage_pw.getText().toString();
                 member_phone = edit_mypage_phone.getText().toString();
                 member_reg = edit_mypage_region.getText().toString();
-
-                postFirebaseDatabase(true);
-                getFirebaseDatabase();
-                setInsertMode();
             }
         });
         
         return fragment;
     }
-    public void postFirebaseDatabase(boolean add){
-        reference = FirebaseDatabase.getInstance().getReference();
-        Map<String, Object> childUpdates = new HashMap<>();
-        Map<String,Object> postValues = null;
-
-        if (add){
-            MembersVO post = new MembersVO(member_id, member_pw, member_phone, member_reg);
-            postValues = post.toMap();
-        }
-        childUpdates.put("/MembersVO/" + member_id, postValues);
-        reference.updateChildren(childUpdates);
-    }
-
-    private void getFirebaseDatabase() {
-        ValueEventListener postListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Log.e("getFirebaseDatabase", "key" + dataSnapshot.getChildrenCount());
-
-                arrayIndex.clear();
-
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
-                    String key = postSnapshot.getKey();
-                    MembersVO get = postSnapshot.getValue(MembersVO.class);
-                    String[] info = {get.member_id, get.member_pw, get.member_phone, get.member_reg};
-
-                    arrayIndex.add(key);
-                    Log.d("getFirebaseDatabase", "key" + key);
-                    Log.d("getFirebaseDatabase", "info" + info[0] + info[1] + info[2] + info[3]);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("getFirebaseDatabase", "loadPost:onCancelled", databaseError.toException());
-            }
-        };
-    }
-//    edit 텍스트 초기화
-    private void setInsertMode() {
-        edit_mypage_pw.setText(member_pw);
-        edit_mypage_phone.setText(member_phone);
-        edit_mypage_region.setText(member_reg);
-    }
-
-
 }

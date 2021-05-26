07708b31a64ea5b4a13e04c2594469209a77f023
joonTeob <jjy8256@gamil.com>
2021-05-26 10:04:03 +0900

    마이페이지에서 포인트로

--- app/src/main/java/com/example/finalproject/MyPageFragment.java
+++ app/src/main/java/com/example/finalproject/MyPageFragment.java
@@ -3,6 +3,7 @@ package com.example.finalproject;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -23,6 +24,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,21 +46,27 @@ public class MyPageFragment extends Fragment {
     ImageView img_mypage_logout;
     RequestQueue requestQueue;
 
+    String updatePw, updatePhone, updateReg;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fragment = inflater.inflate(R.layout.fragment_my_page, container, false);
 
-
         tv_mypage_id = fragment.findViewById(R.id.tv_mypage_id);
         tv_mypage_point = fragment.findViewById(R.id.tv_mypage_point);
+
         edit_mypage_pw = fragment.findViewById(R.id.edit_mypage_pw);
         edit_mypage_phone = fragment.findViewById(R.id.edit_mypage_phone);
         edit_mypage_region = fragment.findViewById(R.id.edit_mypage_region);
         btn_mypage_point = fragment.findViewById(R.id.btn_mypage_point);
         btn_mypage_update = fragment.findViewById(R.id.btn_mypage_update);
         img_mypage_logout = fragment.findViewById(R.id.img_mypage_logout);
+
+        // 요청 보내주는 객체 생성
+        requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.start();
 
         Bundle extra = getArguments();
         if(extra != null){
@@ -73,6 +85,8 @@ public class MyPageFragment extends Fragment {
             edit_mypage_pw.setText(loginPw);
             edit_mypage_phone.setText(loginPhone);
             edit_mypage_region.setText(loginReg);
+
+
 
         }
 
@@ -100,50 +114,7 @@ public class MyPageFragment extends Fragment {
         btn_mypage_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String update_pw = edit_mypage_pw.getText().toString();
-                String update_phone = edit_mypage_phone.getText().toString();
-                String update_reg = edit_mypage_region.getText().toString();
 
-                if (update_pw.length() == 0||update_phone.length() == 0||update_reg.length() == 0){
-                    Toast.makeText(getContext(), "필수 입력값을 작성해주세요!", Toast.LENGTH_SHORT).show();
-                }
-
-                String server_url = "http://222.102.43.79:8088/AndroidServer/JoinController";
-
-                StringRequest request = new StringRequest(
-                        Request.Method.POST, server_url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // 변경 성공시 response 변수에 1값이 저장, 실패시 0
-                        if (response.equals("1")) {
-                            Toast.makeText(getContext(), "수정이 완료되었습니다.", Toast.LENGTH_SHORT).show();
-                        } else if (response.equals("0")) {
-                            Toast.makeText(getContext(), "수정 실패.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.v("hhd", error.getMessage());
-                            }
-                        }
-                ){
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> params = new HashMap<String, String>();
-
-                        //키값 전송
-                        params.put("join_id",loginId);
-                        params.put("join_pw",update_pw);
-                        params.put("join_phone",update_phone);
-                        params.put("join_region",update_reg);
-
-                        return params;
-                    }
-                };
-                requestQueue.(request);
-
-                Log.v("hhd", "Update Click");
             }
         });
 
